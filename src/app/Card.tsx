44007340e1a23599f018565836099eb98d933f3e
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-25 16:38:34 +0530

    adding the dynamic data

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,15 +1,16 @@
 import Image from "next/image"
 import Link from "next/link"
 
-function Card() {
+function Card({ item }) {
 
+  console.log(item)
   return (
 
     <div className="max-w-full bg-white dark:bg-gray-800">
 
 
       <Link href="#">
-        <Image className="rounded-lg p-3" width={1000} height={324} src="/moon.jpg" alt="moon" />
+        <Image className="rounded-lg p-3" width={1000} height={324} src={item.feature_image} alt="moon" />
       </Link>
 
 
@@ -18,13 +19,13 @@ function Card() {
         <div className="flex mb-3">
           <p className="text-sm text-gray-500 dark:text-gray-400"> Mar 10, 2023 </p>
           <p className="text-sm text-gray-500 dark:text-gray-400 mx-1"> , </p>
-          <p className="text-sm text-gray-500 dark:text-gray-400"> 5 min read </p>
+          <p className="text-sm text-gray-500 dark:text-gray-400"> {item.reading_time} min read </p>
         </div>
 
 
-        <Link href="/read/how-are-you">
+        <Link href={`/read/${item.slug}`}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-            Noteworthy technology acquisitions 2021
+            {item.title}
           </h5>
         </Link>
 
