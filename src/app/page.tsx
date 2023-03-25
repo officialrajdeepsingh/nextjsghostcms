de3a999531486cc9e1170b9df66632481ef299f1
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-25 16:40:46 +0530

    update the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,20 @@
 
-import { getPosts } from "./ghost-cli"
+import { getPosts } from "./ghost-client"
 import Card from './Card'
 
 export default async function Home() {
 
   const getPost = await getPosts()
 
-console.log(
-);
-
   return (
     <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
 
       {
         getPost?.map(
-          item=>{
-         return <Card item={item} />
-        })
+          item => {
+            return <Card key={item.uuid} item={item} />
+          })
       }
-          </main >
+    </main >
   )
 }
