524a8595e69921f08fcd24ffc9bb0edf069d31d3
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-26 18:11:52 +0530

    update the reading page

--- src/app/read/[slug]/page.tsx
+++ src/app/read/[slug]/page.tsx
@@ -1,20 +1,36 @@
-import MorePost from "../../MorePost";
+// import MorePost from "../../MorePost";
 import Newsletter from "../../Newsletter";
 import Link from "next/link";
 import { getSinglePost } from "../../ghost-client"
-import { useRouter } from 'next/router'
 import Image from "next/image";
 import Script from 'next/script'
+import { FaAngleLeft } from "react-icons/fa";
+import { formatDate } from "../../utility";
+
 async function Read({ params }: { params: { slug: string }; }) {
 
   const getPost = await getSinglePost(params.slug)
+
 
   return (
     <> <Script src="https://cdn.jsdelivr.net/ghost/portal@~2.25/umd/portal.min.js" />
       <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
 
         <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+
           <article className="mx-auto w-full max-w-3xl format format-xl sm:format-base  format-blue dark:format-invert">
+
+            <div className="flex my-2 w-full justify-between">
+
+              <Link className="inline-flex items-center" href={`/`}>
+                <FaAngleLeft /> Back
+              </Link>
+
+              <Link href={`/tags/${getPost.primary_tag.slug}`}>
+                # {getPost.primary_tag.name}
+              </Link>
+
+            </div>
 
             <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
               {getPost.title}
@@ -30,17 +46,17 @@ async function Read({ params }: { params: { slug: string }; }) {
 
                 <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
 
-                  <img className="mr-4 w-10 h-10 rounded-full" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Jese Leos" />
+                  <Image width={32} height={32} className="mr-4 w-10 h-10 rounded-full" src={getPost.primary_author.profile_image} alt={getPost.primary_author.name} />
 
-                  <Link href="/authors/rajdeepsingh" rel="author" className="text-xl font-bold text-gray-900 dark:text-white">Jese Leos</Link>
+                  <Link href={`/authors/${getPost.primary_author.slug}`} rel="author" className="text-xl font-bold text-gray-900 dark:text-white">{getPost.primary_author.name}</Link>
 
                   <time className="text-base font-light text-gray-500 dark:text-gray-400 mx-1" dateTime={"2022-02-08".toString()} title="February 8th, 2022">
-                    Feb. 8, 2022
+                    {formatDate(getPost.published_at)}
                   </time>
 
                   <div className="text-base w-1 h-1 rounded-full bg-black dark:bg-white mx-1"></div>
 
-                  <p className="text-base font-light text-gray-500 dark:text-gray-400">3 Min Read</p>
+                  <p className="text-base font-light text-gray-500 dark:text-gray-400"> {getPost.reading_time}  Min Read</p>
 
                 </div>
 
