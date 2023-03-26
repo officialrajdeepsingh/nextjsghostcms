050c9843bf24dbc48b2f831023ed5e96d1d6efb7
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-26 18:15:24 +0530

    search not working yet

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -1,22 +1,35 @@
 "use client"
-import React from 'react';
+import React, { use, useEffect, useState } from 'react';
 import * as Popover from '@radix-ui/react-popover';
 import { FaSearch } from "react-icons/fa";
 import Link from 'next/link';
+import { getSearchPosts } from './ghost-client';
+
+// import { getSearchPosts } from "./ghost-client"
 
 function Search() {
+
+  const [inputText, setInputText] = useState(null)
+
+  useEffect(() => {
+
+    //    console.log(use(getSearchPosts("my")))
+
+  }, [inputText])
+
 
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
         <button
           className="cursor-pointer outline-none"
-          aria-label="Update dimensions"
+          aria-label="Search"
         >
           <FaSearch />
 
         </button>
       </Popover.Trigger>
+
       <Popover.Portal>
 
         <Popover.Content
@@ -30,8 +43,10 @@ function Search() {
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
               </div>
-              <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+              <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search here ..." required />
+
               <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700">Search</button>
+
             </div>
           </div>
 
@@ -44,9 +59,13 @@ function Search() {
                 What Is MdBook, And How To Install MdBook CLI?
               </Link>
             </div>
+
           </div>
-        </Popover.Content >
-      </Popover.Portal >
+
+        </Popover.Content>
+
+      </Popover.Portal>
+
     </Popover.Root >
   )
 }
