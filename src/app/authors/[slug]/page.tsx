1709d5c93550c83d9fd3902568251766c15eda6e
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-26 18:04:04 +0530

    remove the icons and convert the img to image component

--- src/app/authors/[slug]/page.tsx
+++ src/app/authors/[slug]/page.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Link from "next/link";
-import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaGlobe } from "react-icons/fa";
+import { FaFacebook, FaTwitter, FaGlobe } from "react-icons/fa";
 import Card from "../../Card"
+import { getSingleAuthor, getSingleAuthorPost } from "../../ghost-client"
+import Image from 'next/image';
 
-function Authors() {
+
+async function Authors({ params }: { params: { slug: string }; }) {
+
+  const getAuthor = await getSingleAuthor(params.slug)
+  const allAuthor = await getSingleAuthorPost(params.slug)
 
   return (
     <>
@@ -12,41 +18,41 @@ function Authors() {
 
           <div className=" p-10 text-gray-500 sm:text-lg dark:text-gray-400">
 
-            <img className="w-36 h-36 p-2 rounded-full mx-auto ring-2 ring-gray-300 dark:ring-gray-500" src="https://flowbite.com/docs/images/people/profile-picture-2.jpg" alt="Bordered avatar" />
+            <Image height={30} width={30} className="w-36 h-36 p-2 rounded-full mx-auto ring-2 ring-gray-300 dark:ring-gray-500" src={getAuthor.profile_image} alt={getAuthor.name} />
 
-            <h2 className="mb-4 mt-4 text-4xl tracking-tight font-bold text-center text-gray-900 dark:text-white">Rajdeep <span className="font-extrabold">Singh</span></h2>
-            <p className="mb-4 font-light text-center">Track work across the enterprise through an open, collaborative platform. Link issues across Jira and ingest data from other software development tools, so your IT support and operations teams have richer contextual information to rapidly respond to requests, incidents, and changes.</p>
+            <h2 className="mb-4 mt-4 text-4xl tracking-tight font-bold text-center text-gray-900 dark:text-white">{getAuthor.name.split(" ")[0]} <span className="font-extrabold">{getAuthor.name?.split(" ")[1]}</span></h2>
+            <p className="mb-4 font-light text-center">{getAuthor.bio} </p>
+
 
             <ul className="flex flex-wrap p-4 justify-center md:space-x-8 md:mt-0 md:text-sm md:font-medium">
 
-              <li>
+              {
+                (getAuthor.website !== null) ? (<li>
+                  <Link href={getAuthor.website} className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
+                    <FaGlobe />
+                  </Link> </li>) : " "
 
-                <Link href={"#"} className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
-                  <FaGlobe />
-                </Link>
 
-              </li>
+              }
 
-              <li>
-                <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
-                  <FaTwitter />
-                </Link>
-              </li>
-              <li>
-                <Link href="#"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded  hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white"> <FaFacebook />
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
-                  <FaLinkedin />
-                </Link>
-              </li>
-              <li>
-                <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
-                  <FaInstagram />
-                </Link>
-              </li>
+              {
+                (getAuthor.twitter !== null) ? (<li>
+                  <Link href={getAuthor.twitter} className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
+                    <FaTwitter />
+                  </Link>
+                </li>) : " "
+              }
+
+              {
+                (getAuthor.facebook !== null) ? (<li>
+                  <Link href={getAuthor.facebook}
+                    className="block py-2 pl-3 pr-4 text-gray-700 rounded  hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white"> <FaFacebook />
+                  </Link>
+                </li>) : " "
+
+              }
+
+
             </ul>
 
           </div>
@@ -57,14 +63,15 @@ function Authors() {
         <div className="px-4 mx-auto max-w-screen-xl">
 
           <h2 className="mb-8 text-2xl font-bold text-gray-900 dark:text-white">
-            More articles from Rajdeep Singh
+            More articles from {getAuthor.name}
           </h2>
 
           <div className="container my-12 mx-auto grid grid-cols-1 gap-12 md:gap-12 lg:gap-12  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4 ">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+
+            {
+              allAuthor.map(item => <Card item={item} />)
+            }
+
           </div>
         </div>
       </aside>
