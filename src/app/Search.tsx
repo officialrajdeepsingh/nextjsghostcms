a00ef4db5d4dc7accb0b9c70495b70dc574aa2d5
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-28 18:17:18 +0530

    importing json file

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -1,23 +1,45 @@
 "use client"
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as Popover from '@radix-ui/react-popover';
 import { FaSearch } from "react-icons/fa";
 import Link from 'next/link';
-import { getSearchPosts } from './ghost-client';
+import searchData from '../../search.json'
 
-// import { getSearchPosts } from "./ghost-client"
+let serachPost = []
+const setSearch = new Set();
 
 function Search() {
 
-  const [inputText, setInputText] = useState(null)
+  const [query, setQuery] = useState("")
+
+  const [data, setData] = useState({
+    item: []
+  })
+
 
   useEffect(() => {
+    searchData.map(
+      (item) => {
+        if (item.title.trim().toLowerCase().includes(query?.trim().toLowerCase())) {
+          setSearch.add(item)
+          serachPost.push(item)
+        }
+      }
+    )
+  }, [query])
 
-    //    console.log(use(getSearchPosts("my")))
+  function submitData() {
 
-  }, [inputText])
+    // searchData.map(
+    //   (item) => {
+    //     if (item.title.trim().toLowerCase().includes(query?.trim().toLowerCase())) {
+    //       setSearch.add(item)
+    //     }
+    //   }
+    // )
 
-
+  }
+  console.log(' setSearch:', data.item)
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
@@ -26,7 +48,6 @@ function Search() {
           aria-label="Search"
         >
           <FaSearch />
-
         </button>
       </Popover.Trigger>
 
@@ -37,30 +58,35 @@ function Search() {
           sideOffset={5}
         >
 
-          <div className='my-2 '>
+          <div className='my-2'>
             <label htmlFor="default-search" className="mb-2 mt-5 text-sm font-medium text-gray-900 sr-only dark:text-white">Search bar </label>
             <div className="relative">
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
               </div>
-              <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search here ..." required />
+              <input type="search" id="default-search" onChange={(event) => setQuery(event?.target.value)} className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search here ..." required />
 
-              <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700">Search</button>
+              <button type="submit" onClick={submitData} className="text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700">Search</button>
 
             </div>
           </div>
 
 
+          {
 
-          <div className='my-3'>
+            serachPost.length > 0 ? serachPost.map(item => {
+              return (
+                <div className='my-3'>
+                  <div className="text-white my-2 py-2 bg-blue-400 dark:bg-gray-900 dark:hover:bg-blue-400 border-none rounded-md dark:text-white">
+                    <Link href={`read/${item.slug}`} className="relative inline-flex items-center rounded-lg w-full px-4 py-2 text-sm font-medium">
+                      {item.title}
+                    </Link>
+                  </div>
+                </div>
+              )
+            }) : " "
 
-            <div className="text-white my-2 py-2 bg-blue-400 dark:bg-gray-900 dark:hover:bg-blue-400 border-none rounded-md dark:text-white">
-              <Link href="/" className="relative inline-flex items-center rounded-lg w-full px-4 py-2 text-sm font-medium">
-                What Is MdBook, And How To Install MdBook CLI?
-              </Link>
-            </div>
-
-          </div>
+          }
 
         </Popover.Content>
 
