f940ea01fe83fb637c94f7e9d1970cdeaf43d1db
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-28 21:05:21 +0530

    remove the unused code

--- src/app/Search.tsx
+++ src/app/Search.tsx
@@ -4,42 +4,30 @@ import * as Popover from '@radix-ui/react-popover';
 import { FaSearch } from "react-icons/fa";
 import Link from 'next/link';
 import searchData from '../../search.json'
+import type { PostOrPage, PostsOrPages } from "tryghost__content-api"
 
-let serachPost = []
-const setSearch = new Set();
+let serachPost: PostOrPage[] = []
 
 function Search() {
 
-  const [query, setQuery] = useState("")
-
-  const [data, setData] = useState({
-    item: []
-  })
-
+  const [query, setQuery] = useState(null)
 
   useEffect(() => {
+
+    serachPost.length = 0;
+
     searchData.map(
-      (item) => {
+      (item: PostsOrPages) => {
+
         if (item.title.trim().toLowerCase().includes(query?.trim().toLowerCase())) {
-          setSearch.add(item)
           serachPost.push(item)
         }
       }
     )
+
   }, [query])
 
-  function submitData() {
 
-    // searchData.map(
-    //   (item) => {
-    //     if (item.title.trim().toLowerCase().includes(query?.trim().toLowerCase())) {
-    //       setSearch.add(item)
-    //     }
-    //   }
-    // )
-
-  }
-  console.log(' setSearch:', data.item)
   return (
     <Popover.Root>
       <Popover.Trigger asChild>
@@ -64,9 +52,7 @@ function Search() {
               <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
               </div>
-              <input type="search" id="default-search" onChange={(event) => setQuery(event?.target.value)} className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search here ..." required />
-
-              <button type="submit" onClick={submitData} className="text-white absolute right-2.5 bottom-2.5 bg-blue-600 hover:bg-blue-800 outline-none font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700">Search</button>
+              <input type="search" id="default-search" onChange={(event) => setQuery(event?.target.value)} className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Start searching here ..." required />
 
             </div>
           </div>
