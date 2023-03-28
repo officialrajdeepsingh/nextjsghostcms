1041ccdff2ab0f3b077928f79155cd5e72cbffcf
officialrajdeepsingh <radewala.2@gmail.com>
2023-03-28 18:15:05 +0530

    enabling the serach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,49 @@
-
 import { getPosts } from "./ghost-client"
 import Card from './Card'
+import Pagination from "./Pagination"
+import * as fs from 'node:fs';
+import { cache } from "react"
 
 
 export default async function Home() {
 
   const getPost = await getPosts()
 
-  return (
-    <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
+  cache(async () => {
 
-      {
-        getPost?.map(
-          item => {
-            return <Card key={item.uuid} item={item} />
-          })
-      }
-    </main >
+    try {
+
+      const jsonString = JSON.stringify(getPost)
+
+      fs.writeFile('search.json', jsonString, 'utf8', err => {
+        if (err) {
+          console.log('Error writing file', err)
+        } else {
+          console.log('Successfully wrote file')
+        }
+      })
+
+    } catch (error) {
+      console.log('error : ', error)
+    }
+
+  })
+
+
+  return (
+    <>
+      <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
+
+        {
+          getPost?.map(
+            item => {
+              return <Card key={item.uuid} item={item} />
+            })
+        }
+
+      </main>
+
+      <Pagination item={getPost.meta.pagination} />
+    </>
   )
 }
