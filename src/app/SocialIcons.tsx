2e87db8842a9b1fa4113eb0b76a71b5a8e415670
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-01 21:49:02 +0530

    adding social icon in app

---
+++ src/app/SocialIcons.tsx
@@ -0,0 +1,53 @@
+"use client"
+
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaSun, FaRegMoon } from "react-icons/fa";
+import { useTheme } from 'next-themes'
+import Link from "next/link";
+
+
+import dynamic from 'next/dynamic'
+
+const Search = dynamic(() => import('./Search'))
+
+function SocialIcons() {
+
+  const { theme, setTheme } = useTheme()
+
+  return <div className="flex md:order-2">
+
+    <ul className="flex flex-wrap p-4 md:space-x-8 md:mt-0 md:text-sm md:font-medium">
+
+      <li className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
+        <Search />
+      </li>
+
+      <li>
+        <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
+          <FaTwitter />
+        </Link>
+      </li>
+      <li>
+        <Link href="#"
+          className="block py-2 pl-3 pr-4 text-gray-700 rounded  hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white"> <FaFacebook />
+        </Link>
+      </li>
+      <li>
+        <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
+          <FaLinkedin />
+        </Link>
+      </li>
+      <li>
+        <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
+          <FaInstagram />
+        </Link>
+      </li>
+      <li>
+        <button className="block py-2 pl-3 pr-4 rounded md:p-0" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+          {theme === "dark" ? <FaSun /> : <FaRegMoon />}
+        </button>
+      </li>
+    </ul>
+  </div>;
+}
+
+export default SocialIcons
