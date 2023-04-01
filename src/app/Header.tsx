66d7fb558ab3b208839648a8c6c3780fda9f6cf2
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-01 21:49:35 +0530

    adding navigation and logo

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,16 +1,9 @@
-"use client"
-
-import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaSun, FaRegMoon } from "react-icons/fa";
-import { useTheme } from 'next-themes'
 import Link from "next/link";
+import SocialIcons from "./SocialIcons";
+import Image from "next/image";
+function Header({ setting }) {
 
-import dynamic from 'next/dynamic'
-
-const Search = dynamic(() => import('./Search'))
-
-function Header() {
-
-  const { theme, setTheme } = useTheme()
+  console.log(setting.logo, "Header : ", setting)
 
   return (
     <header className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full">
@@ -18,49 +11,28 @@ function Header() {
       <div className="container flex flex-wrap items-center justify-between mx-auto">
 
         <Link href="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">OpenBlog</span>
+          <Image alt={setting.title} width={200} height={100} src={setting.logo} className="self-center text-xl font-semibold whitespace-nowrap dark:text-white" />
         </Link>
-
         <div className="flex md:order-2">
 
           <ul className="flex flex-wrap p-4 md:space-x-8 md:mt-0 md:text-sm md:font-medium">
 
-            <li className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
-              <Search />
-            </li>
+            {
+              setting.navigation.map(item => <li key={item.label} className="block py-2 pl-3 pr-4 text-gray-700 hover:text-blue-700 dark:hover:text-blue-700 rounded md:p-0 dark:text-white" aria-current="page">
+                <Link href={item.url}>
+                  {item.label}
+                </Link>
+              </li>)
 
-            <li>
-              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white" aria-current="page">
-                <FaTwitter />
-              </Link>
-            </li>
-            <li>
-              <Link href="#"
-                className="block py-2 pl-3 pr-4 text-gray-700 rounded  hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white"> <FaFacebook />
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
-                <FaLinkedin />
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:text-blue-700 dark:hover:text-blue-700 md:p-0 dark:text-white">
-                <FaInstagram />
-              </Link>
-            </li>
-            <li >
-              <button className="block py-2 pl-3 pr-4 rounded md:p-0" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-                {theme === "dark" ? <FaSun /> : <FaRegMoon />}
-              </button>
-            </li>
+            }
+
           </ul>
-        </div>
 
+        </div>
+        <SocialIcons />
       </div>
 
     </header >
-
   )
 
 }
