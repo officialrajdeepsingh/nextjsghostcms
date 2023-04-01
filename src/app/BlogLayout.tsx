db8296622a850a8227ffe0004ff462c5e7d4ce1c
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-01 21:50:01 +0530

    pass the navigation data to header component

--- src/app/BlogLayout.tsx
+++ src/app/BlogLayout.tsx
@@ -1,14 +1,15 @@
 "use client"
+
+
 import Footer from "./Footer";
 import Header from "./Header"
 import { ThemeProvider } from 'next-themes'
 
-function Layout({ children }: {
+function Layout({ setting, children }: {
   children: React.ReactNode
 }) {
-
   return <ThemeProvider attribute="class">
-    <Header />
+    <Header setting={setting} />
     {children}
     <Footer />
   </ThemeProvider>
