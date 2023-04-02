84c04594dc7e46e1ccc47ab5c9e42ca07fb481bd
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-02 11:27:31 +0530

    remove the background color in footer

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -6,12 +6,12 @@ function Footer() {
 
   return (
 
-    <footer className="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full">
+    <footer className="px-2 sm:px-4 py-2.5 dark:bg-gray-900 w-full">
 
       <div className="container flex flex-wrap items-center justify-between mx-auto">
 
         <Link href="https://github.com/frontendweb3" className="flex items-center">
-          <span className="self-center text-gray-400 text-sm font-semibold whitespace-nowrap dark:text-white">2023 copyright frontend web</span>
+          <span className="self-center text-gray-800 text-sm font-semibold whitespace-nowrap dark:text-white">2023 copyright frontend web</span>
         </Link>
 
         <div className="flex md:order-2">
