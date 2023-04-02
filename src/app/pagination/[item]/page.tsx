1cd4140ab1fbe2d581265a5e04c032492d4e9e81
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-02 13:45:46 +0530

    adding dynamic meta data in generateMetadata

--- src/app/pagination/[item]/page.tsx
+++ src/app/pagination/[item]/page.tsx
@@ -1,6 +1,22 @@
-import { getPaginationPosts, getPosts } from "../../ghost-client"
+import { getPaginationPosts, getPosts, getNavigation } from "../../ghost-client"
 import Card from '../../Card'
 import PaginationItem from "../../Pagination"
+import type { Metadata } from "next";
+export async function generateMetadata({ params }: { params: { slug: string }; }): Metadata {
+
+  const metaData = await getNavigation()
+
+  return {
+    title: metaData.title,
+    description: metaData.description,
+    openGraph: {
+      title: metaData.title,
+      description: metaData.excpet,
+      url: metaData.url,
+    },
+  }
+}
+
 
 export async function generateStaticParams() {
 
@@ -24,14 +40,14 @@ export async function generateStaticParams() {
 
 export default async function Pagination({ params }: { params: { item: string }; }) {
 
+  let getParams = Number.parseInt(params.item)
 
-  const getPost = await getPaginationPosts(2)
-
+  const getPost = await getPaginationPosts(getParams)
 
   return (
     <>
 
-      <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4  lg:grid-cols-3  md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
+      <main className="container my-12 mx-auto grid grid-cols-1 gap-2 md:gap-3 lg:gap-4 lg:grid-cols-3 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-4">
 
         {
           getPost?.map(
