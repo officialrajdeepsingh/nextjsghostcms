2cdf5b370a0ad70e12324e32a6f42b4631172a66
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-03 16:35:02 +0530

    Replace the utility with date-fns

--- src/app/Card.tsx
+++ src/app/Card.tsx
@@ -1,24 +1,28 @@
+// @ts-nocheck
 import Image from "next/image"
 import Link from "next/link"
-import { formatDate } from "./utility"
+import type { PostOrPage } from "@tryghost/content-api";
+import { format } from 'date-fns'
 
 function Card({ item }: { item: PostOrPage }) {
 
   return (
     <div className="max-w-full bg-white dark:bg-gray-800" >
 
-
-      <Link href={`/read/${item.slug}`}>
-        <Image className="rounded-lg p-3" width={1000} height={324} src={item.feature_image} alt={item.title} />
-      </Link>
-
+      {
+        item.featured !== null && item.feature_image !== undefined ? <Link href={`/read/${item.slug}`}>
+          <Image className="rounded-lg p-3" width={1000} height={324} src={item.feature_image} alt={item.feature_image_alt || item.title} />
+        </Link> : " "
+      }
 
       <div className="p-3">
 
         <div className="flex mb-3">
-          <p className="text-sm text-gray-500 dark:text-gray-400">
-            {formatDate(item.published_at)}
-          </p>
+          {
+            item.published_at !== null && item.published_at !== undefined ? <p className="text-sm text-gray-500 dark:text-gray-400">
+              {format(new Date(item.published_at), 'dd MMMM, yyyy')}
+            </p> : ""
+          }
           <p className="text-sm text-gray-500 dark:text-gray-400 mx-1"> , </p>
           <p className="text-sm text-gray-500 dark:text-gray-400">
             {item.reading_time} min read
