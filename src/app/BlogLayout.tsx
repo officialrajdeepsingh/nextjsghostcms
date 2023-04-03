496f09bc7fbe4273e21aa8dda6a8a62df8a1f8b6
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-03 17:25:00 +0530

    fix the Settings import @tryghost/content-api

--- src/app/BlogLayout.tsx
+++ src/app/BlogLayout.tsx
@@ -1,9 +1,8 @@
 "use client"
 import Footer from "./Footer";
-import Header from "./Header"
-import { ThemeProvider } from 'next-themes'
-import type { Settings } from "tryghost__content-api"
-
+import Header from "./Header";
+import { ThemeProvider } from 'next-themes';
+import type { Settings } from "@tryghost/content-api";
 function Layout({ setting, children }: { setting: Settings, children: React.ReactNode }) {
   return <ThemeProvider attribute="class">
     <Header setting={setting} />
