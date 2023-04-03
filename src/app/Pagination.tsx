76f6d365744a4f1b7177c49e81000f7a9274487a
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-03 16:32:55 +0530

    adding Pagination type

--- src/app/Pagination.tsx
+++ src/app/Pagination.tsx
@@ -1,48 +1,50 @@
 import Link from "next/link"
+import { Pagination } from "@tryghost/content-api"
 
+function PaginationItem({ item }: { item: Pagination }) {
 
-function Pagination({ item }) {
+  let paginationItems = []
 
-  let paginationItem = []
-
-   for (let index = 1; index <= item?.pages; index++) {
-    paginationItem.push(<li key={index*2} ><Link href={index === 1 ? "/" : `/pagination/${index}`} className="px-3 py-2 leading-tight bg-blue-100 hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">
+  for (let index = 1; index <= item?.pages; index++) {
+    paginationItems.push(<li key={index * 2} ><Link href={index === 1 ? "/" : `/pagination/${index}`} className="px-3 py-2 leading-tight bg-blue-100 hover:bg-blue-200 border-transparent border rounded-lg text-black dark:bg-gray-800 dark:text-gray-400 mx-2 dark:hover:bg-gray-700 dark:hover:text-white">
       {index}
     </Link></li>)
-   }
+  }
 
-return (
+  return (
 
-  <nav aria-label="pagination" className="mx-auto my-20 container">
+    <nav aria-label="pagination" className="mx-auto my-20 container">
 
-    <ul className="mx-auto flex justify-center -space-x-px">
+      <ul className="mx-auto flex justify-center -space-x-px">
 
-      <li>
-        {
-          item.prev ? <Link href={item.prev === 1 ? "/" : `/pagination/${item.prev}`} className="px-3 py-2 mr-2 border border-transparent rounded-md  leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
+        <li>
+          {
+            item.prev ? <Link href={item.prev === 1 ? "/" : `/pagination/${item.prev}`} className="px-3 py-2 mr-2 border border-transparent rounded-md  leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
               dark:hover:bg-gray-700 dark:hover:text-white">
-            Prev
-          </Link> : " "
-        }
-      </li>
-      {paginationItem}
-      <li>
-        {
-          item.next ? <Link href={`/pagination/${item.next}`} className="px-3 py-2 ml-2 border border-transparent rounded-md leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
+              Prev
+            </Link> : " "
+          }
+        </li>
+
+        {paginationItems}
+
+        <li>
+          {
+            item.next ? <Link href={`/pagination/${item.next}`} className="px-3 py-2 ml-2 border border-transparent rounded-md leading-tight bg-white hover:text-blue-700 dark:bg-gray-800 dark:text-gray-400
             dark:hover:bg-gray-700 dark:hover:text-white">
-            Next
-          </Link> : " "
-        }
-      </li>
+              Next
+            </Link> : " "
+          }
+        </li>
 
 
-    </ul>
+      </ul>
 
-  </nav>
+    </nav>
 
-)
+  )
 
 }
 
 
-export default Pagination
+export default PaginationItem
