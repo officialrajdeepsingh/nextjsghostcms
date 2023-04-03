d2dade7e2eb9829a23101f7d84c185905f34a3c4
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-03 16:26:51 +0530

    update the component

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,10 +6,10 @@ function NotFound() {
     <section className="bg-white dark:bg-gray-900 my-16">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
-          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white"> Something's wrong</p>
+          <h1 className="mb-4 text-7xl tracking-tight lg:text-9xl text-primary-600 dark:text-primary-500">404</h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white"> Something wrong</p>
           <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">
-            Sorry, we can't find that article. You'll find lots to explore on the home page.
+            Sorry, we cant find that article. You will find lots to explore on the home page.
           </p>
           <Link href="/" className="inline-flex text-white bg-black dark:bg-white dark:text-black p-3 hover:bg-gray-800 my-4">Back to Homepage</Link>
         </div>
