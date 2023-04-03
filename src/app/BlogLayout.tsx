41ac32f58d21b6413967fc42bba64646e859191f
officialrajdeepsingh <radewala.2@gmail.com>
2023-04-03 16:37:06 +0530

    adding type with props

--- src/app/BlogLayout.tsx
+++ src/app/BlogLayout.tsx
@@ -1,13 +1,10 @@
 "use client"
-
-
 import Footer from "./Footer";
 import Header from "./Header"
 import { ThemeProvider } from 'next-themes'
+import type { Settings } from "tryghost__content-api"
 
-function Layout({ setting, children }: {
-  children: React.ReactNode
-}) {
+function Layout({ setting, children }: { setting: Settings, children: React.ReactNode }) {
   return <ThemeProvider attribute="class">
     <Header setting={setting} />
     {children}
